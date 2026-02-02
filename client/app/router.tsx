714a41bc2f9a20da5814d1093212d415b52841f4
BenDaBeast22 <bencritoph@gmail.com>
2026-02-02 16:31:53 -0500

    change client from ssr to browser

---
+++ client/app/router.tsx
@@ -0,0 +1,25 @@
+import { createBrowserRouter } from "react-router-dom";
+
+// Import your pages/components
+import Home from "./routes/home";
+import Menu from "./routes/menu";
+import TestSocket from "./routes/test-socket";
+import Game from "./routes/game";
+import MultiplayerSetup from "./routes/multiplayer-setup";
+import HostGame from "./routes/host-game";
+import JoinGame from "./routes/join-game";
+import Lobby from "./routes/lobby";
+import NotFound from "./routes/not-found";
+
+export const router = createBrowserRouter([
+  { path: "/", element: <Home /> },                       // Home page
+  { path: "/menu", element: <Menu /> },
+  { path: "/test-socket", element: <TestSocket /> },
+  { path: "/game", element: <Game /> },                   // Local game
+  { path: "/game/:lobbyId", element: <Game /> },          // Multiplayer game
+  { path: "/multiplayer-setup", element: <MultiplayerSetup /> },
+  { path: "/host-game", element: <HostGame /> },
+  { path: "/join-game", element: <JoinGame /> },
+  { path: "/lobby/*", element: <Lobby /> },               // wildcard for lobby sub-routes
+  { path: "*", element: <NotFound /> },                  // catch-all 404
+]);
