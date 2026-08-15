6c00f2ed92604e6fc408a910e68d3201abe35148
Jeffrey Wang <jeffrey.19jw@gmail.com>
2026-08-15 00:52:54 -0400

    speed up line reveal by 100ms

--- client/app/ui/Game/RoundScore.tsx
+++ client/app/ui/Game/RoundScore.tsx
@@ -17,7 +17,7 @@ type ChildProps = {
   cardSizes: CardSizesType;
 };
 
-const LINE_REVEAL_DELAY_MS = 900;
+const LINE_REVEAL_DELAY_MS = 800;
 const RACE_DURATION_MS = 1400;
 const RACE_TICKS = 28;
 
