1f3f708052cf911ae957f3777e6ee19a13e60798
BenDaBeast22 <bencritoph@gmail.com>
2026-08-22 19:22:21 -0400

    test

--- client/app/connections/socket.ts
+++ client/app/connections/socket.ts
@@ -2,7 +2,7 @@
 import { io } from "socket.io-client";
 
 // export const socket = io();
-const socketUrl = import.meta.env.VITE_BACKEND_URL || "http://localhost:4000";
+const socketUrl = import.meta.env.VITE_BACKEND_URL || "";
 export const socket = io(socketUrl);
 
 // optional: log connection once
